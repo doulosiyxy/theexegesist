81c97d49143e29433ff14b75dfae7e581c0fa0c2
doulosiyxy <31935830+doulosiyxy@users.noreply.github.com>
2017-11-18 18:06:54 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-$(document).ready( function() {
-  $('.title').delay(19500).fadeTo("2000", 1);
-  $('#enter-btn').hide().delay(19500).fadeIn(700);
 
-//below is for text change on enter btn.
-
-  $("#enter-btn").on('mouseenter', () => {
-  $("#enter-btn").text("If You Dare");
-  });
-  
-  $("#enter-btn").on('mouseleave', () => {
-  $("#enter-btn").text("Enter");
-  });
-  
-  
-});
