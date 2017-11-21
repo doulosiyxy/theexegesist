1088fdf02c7ddf1e5e814e90004f8792f7f40ee1
doulosiyxy <31935830+doulosiyxy@users.noreply.github.com>
2017-11-21 10:57:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
   $('.title').delay(19500).fadeTo("2000", 1);
   $('#enter-btn').hide().delay(19500).fadeIn(700);
-  $('#close').delay(2000).fadeTo(2000, 1).delay(15500).fadeOut(2000);
+  $('#close-wrapper').delay(2000).fadeTo(2000, 1).delay(15500).fadeOut(2000);
 
 //below is for text change on enter btn.
 
