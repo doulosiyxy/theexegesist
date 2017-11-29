6ee23068c854ed8147742cdcfccee8057e21a26d
doulosiyxy <31935830+doulosiyxy@users.noreply.github.com>
2017-11-29 18:18:08 +0000

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ $(document).ready( function() {
   setInterval(flicker, 10000);
   
   $('#post-img').on('click', () => {
-  	flicker();
+  	$('#post-img').fadeTo(5, 0).delay(50).fadeTo(100, 1).delay(50).fadeTo(5, 0).delay(1000).fadeTo(100, 1);
   }); 
   
   //popups
