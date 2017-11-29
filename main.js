15444e2bede33dc16ee11480d4214e9e7f3cea31
doulosiyxy <31935830+doulosiyxy@users.noreply.github.com>
2017-11-29 20:12:09 +0000

    Update main.js

--- main.js
+++ main.js
@@ -39,14 +39,14 @@ $(document).ready( function() {
   //#post-img animation
   
   function flicker() { 
-    $('#post-img').fadeTo(5, 0).delay(50).fadeTo(100, 1).delay(50).fadeTo(5, 0).delay(1000).fadeTo(100, 1); 
+    $('.post-img').fadeTo(5, 0).delay(50).fadeTo(100, 1).delay(50).fadeTo(5, 0).delay(1000).fadeTo(100, 1); 
  
     }
     
   setInterval(flicker, 10000);
   
-  $('#post-img').on('click', () => {
-  	$('#post-img').fadeTo(5, 0).delay(50).fadeTo(100, 1).delay(50).fadeTo(5, 0).delay(1000).fadeTo(100, 1);
+  $('.post-img').on('click', () => {
+  	$('.post-img').fadeTo(5, 0).delay(50).fadeTo(100, 1).delay(50).fadeTo(5, 0).delay(1000).fadeTo(100, 1);
   }); 
   
   //popups
